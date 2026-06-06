48f4eac92ba301f844b5106d0ddd9e7c9e048adc
haganirami-png <haganirami@gmail.com>
2026-06-06 06:10:44 +0300

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import SportMatchFull1 from './SportMatchFull1'
+import SportMatchFull1 from './SportMatchFull'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
