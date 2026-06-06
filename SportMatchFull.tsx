edf73c7b3656468f990d4aa58136bb75ac84c489
haganirami-png <haganirami@gmail.com>
2026-06-06 21:38:11 +0300

    Update SportMatchFull.tsx

--- SportMatchFull.tsx
+++ SportMatchFull.tsx
@@ -1347,7 +1347,7 @@ async function searchPlayer(data) {
   const last = data?.last || "";
   const year = data?.year || "";
 
-  const apiUrl = "https://hammer-involves-magic-concrete.trycloudflare.com/search?" + new URLSearchParams({
+  const apiUrl = "https://arising-targeted-techniques-mainstream.trycloudflare.com/search?" + new URLSearchParams({
     first,
     last,
     year,
