69b339ce8fedfa4cdaf040e046bd42d56c1d2586
haganirami-png <haganirami@gmail.com>
2026-06-06 06:04:18 +0300

    Add files via upload

---
+++ main.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import SportMatchFull1 from './SportMatchFull1'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <SportMatchFull1 />
+  </React.StrictMode>
+)
